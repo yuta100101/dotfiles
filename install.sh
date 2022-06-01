f556abfa25d4bda24432f1649ca45c566d4e33a5
yuta100101 <uchida.yuta.mar.sh.nat@gmail.com>
2022-06-02 00:48:19 +0900

    Add: install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DOTFILE_DIR=$(cd $(dirname $0) | pwd)
+
+mkdir ~/.bachup
+
+dotfiles=(".bashrc" ".bash_aliases" ".gitignore")
+
+for dotfile in ${dotfiles[@]}; do
+    if [ -f $dotfile ]; then
+        mv ~/$dotfile ~/.bachup
+    ln -s $DOTFILE_DIR/$dotfile ~
