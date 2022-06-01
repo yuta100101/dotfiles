531276e32890416b688092434f68d486c4d40e9e
yuta100101 <uchida.yuta.mar.sh.nat@gmail.com>
2022-06-02 00:53:35 +0900

    Fix: install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir ~/.bachup
 dotfiles=(".bashrc" ".bash_aliases" ".gitignore")
 
 for dotfile in ${dotfiles[@]}; do
-    if [ -f $dotfile ]; then
+    if [ -f ~/$dotfile ]; then
         mv ~/$dotfile ~/.bachup
     fi
     ln -s $DOTFILE_DIR/$dotfile ~
